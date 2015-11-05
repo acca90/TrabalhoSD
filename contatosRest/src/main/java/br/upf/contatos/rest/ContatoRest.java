264fbd5f1e687dc46f791e3ea049066435ecbc53
JonasMartovicz <jonasmartovicz@hotmail.com>
2015-11-04 23:12:18 -0200

    Modificações do código, principalmente com retorno de status da requisição.

--- contatosRest/src/main/java/br/upf/contatos/rest/ContatoRest.java
+++ contatosRest/src/main/java/br/upf/contatos/rest/ContatoRest.java
@@ -11,62 +11,107 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
-
-/**
- *
- * @author Mauricley
- */
+import javax.ws.rs.core.Response;
 
 @Path("contatos")
 public class ContatoRest {
     
     private ContatoService service = new ContatoService();
     
+    /**
+     * Consulta: retorna os dados de um contato. Caso o contato não exista no BD, retornar essa informação
+     * @return 
+     */
     @GET
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Contato> getAll() {
-        
-        return service.getAll();
+    public Response getAll() {
+        return Response.ok(new GenericEntity<List<Contato>>(service.getAll()){}).build();
     }
     
+    /**
+     * Consulta: retorna os dados de um contato. Caso o contato não exista no BD, retornar essa informação
+     * @param id
+     * @return 
+     */
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Contato getById(@PathParam("id") Integer id) {
-        return service.getById(id);
+    public Response getById(@PathParam("id") Integer id) {
+        return Response.ok(new GenericEntity<Contato>(service.getById(id)){}).build();
     }
     
+    
+    /**
+     * ListaCidade: deve ser recebida a cidade e retornar uma lista contendo o código, nome e e-mail de todos os contatos no BD de uma determinada cidade
+     * @param cidade
+     * @return 
+     */
     @GET
     @Path("c-{cidade}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Contato> getById(@PathParam("cidade") String cidade) {
-        return service.getByCidade(cidade);
+    public Response getByCidade(@PathParam("cidade") String cidade) {
+        return Response.ok(new GenericEntity<List<Contato>>(service.getByCidade(cidade)){}).build();
     }
+
     
+    /**
+     * Adiciona: adiciona contatos ao BD. Caso código do contato ou e-mail já exista, não adicionar e informar um erro
+     * @param c
+     * @return 
+     */
     @POST
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Contato insert(Contato c) {
-        return service.add(c);
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response insert(Contato c) {
+        if(service.add(c) != null)
+            return Response.ok(new GenericEntity<List<Contato>>(service.getAll()){}).build();
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new GenericEntity<List<Contato>>(service.getAll()){}).build();
     }
     
+    /**
+     * Altera: altera contatos no BD. Deve-se informar o contato, o dado a ser alterado e seu novo valor. Caso o código do contato não exista, retornar essa informação
+     * Ainda tem um problema, em vez de atualizar, está duplicando o contato no banco de dados
+     * @param c
+     * @return 
+     */
     @PUT
     @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Contato update(Contato c) {
-        return service.update(c);
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Response update(Contato c) {
+        if(service.update(c) != null)
+            return Response.ok(new GenericEntity<List<Contato>>(service.getAll()){}).build();
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new GenericEntity<List<Contato>>(service.getAll()){}).build();
     }
     
+    /**
+     * Excluir: apaga um contato no BD. Deve-se informar o código do contato. Caso o contato não exista no BD, retornar essa informação
+     * @param id
+     * @return 
+     */
     @DELETE
     @Path("{id}")
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Contato delete(@PathParam("id") Integer id) {
-        return service.delete(id);
+    public Response delete(@PathParam("id") Integer id) {
+        if(service.delete(id) != null)
+            return Response.ok(new GenericEntity<List<Contato>>(service.getAll()){}).build();
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new GenericEntity<List<Contato>>(service.getAll()){}).build();
     }
     
-    @DELETE
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Contato delete(Contato c) {
-        return service.delete(c);
-    }
+    /**
+     * Excluir: apaga um contato no BD. Deve-se informar o código do contato. Caso o contato não exista no BD, retornar essa informação
+     * Esse código não funciona. Pesquisei, e acho que o DELETE não aceita nenhum tipo de entrada no corpo da mensagem, apenas saída
+     * @param c
+     * @return 
+     *
+     * @DELETE
+     * @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+     * @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+     * public Response delete(Contato c) {
+     *     if(service.delete(c) != null)
+     *         return Response.ok(new GenericEntity<List<Contato>>(service.getAll()){}).build();
+     *     return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new GenericEntity<List<Contato>>(service.getAll()){}).build();
+     * }
+    */
 }
