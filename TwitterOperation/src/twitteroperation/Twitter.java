b04840b9a40557b44f504674d4973ab790778f02
Camila Posser <camila_posser@hotmail.com>
2015-11-14 00:14:29 -0200

    Integração com o twitter 100% funcionando
    Criei uma conta para o trabalho: trabalhosdupf@gmail.com e senha:
    trabalhosdupf

--- TwitterOperation/src/twitteroperation/Twitter.java
+++ TwitterOperation/src/twitteroperation/Twitter.java
@@ -5,30 +5,23 @@
 package twitteroperation;
 
 import java.util.List;
+import java.util.Scanner;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
-/**
- *
- * @author dell
- */
-public class TwitterOperation {
+public class Twitter {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void twitter(String[] args) throws TwitterException {
-        
-        
+    public static void main(String[] args) throws TwitterException {
+
           ConfigurationBuilder cb = new ConfigurationBuilder();
           
            cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("zQ9a9Peguity7uhQL1NEsQoOc")
-                .setOAuthConsumerSecret("XCHvfk7cXJqzs936oE9P8n9phAYdS6LeKrxtTeup8E4dbHue1m")
-                .setOAuthAccessToken("66044069-IVRp6vydYkBSdjo2zs78MmkmuFSC0bqdw0a2U6CYS")
-                .setOAuthAccessTokenSecret("uboCYpkXzC19rYwzor722SVd7yj2jVlOc6ajVSikOrMoL");
+                .setOAuthConsumerKey("Tzi51y4WZtyjWyRnjl8kCB6B2")
+                .setOAuthConsumerSecret("fpOK1IQxDtFPkbBUnYsQ8fowE3tSkbZmKR6SfJ9grPhkmHcKTH")
+                .setOAuthAccessToken("4229537124-XFDAf1RDS5Skm0ovVrTQ0HsefDxBECqWpuNMCou")
+                .setOAuthAccessTokenSecret("SmiJP4ov6VJ4owOixjduE2QIWAlaVz0hR5I3p5egvAuCm");
            
      TwitterFactory tf=new TwitterFactory(cb.build());  
      
@@ -36,8 +29,11 @@ public class TwitterOperation {
      
      
      // Post on twitter time line .
-     
-    Status stat= tw.updateStatus("Welcome to twitter world @@@@@ ");
+     String nome;
+     Scanner sler = new Scanner (System.in); 
+     System.out.println("digite"); 
+     nome = sler.next();
+     Status stat= tw.updateStatus(nome);
     
       System.out.println("Twitter updated");
         
@@ -50,15 +46,6 @@ public class TwitterOperation {
           //  System.out.println(status1.getUser().getName() + ":" + status1.getText());
 
       //  }
-        
-        
-        
-        
-           
-           
-           
 
-        
-        
     }
 }
