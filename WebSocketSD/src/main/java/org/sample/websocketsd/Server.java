882b8be756fab1f9e47d17101806b077a69a0865
Camila Posser <camila_posser@hotmail.com>
2015-11-20 17:25:36 -0200

    Inicio WebSocket

---
+++ WebSocketSD/src/main/java/org/sample/websocketsd/Server.java
@@ -0,0 +1,61 @@
+package org.sample.websocketsd;
+
+/**
+ *
+ * @author Camila
+ */
+import br.upf.contatos.dal.model.Contato;
+import br.upf.contatos.dal.service.ContatoService;
+import java.io.IOException;
+ 
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+ 
+/** 
+ * @ServerEndpoint gives the relative name for the end point
+ * This will be accessed via ws://localhost:8080/EchoChamber/echo
+ * Where "localhost" is the address of the host,
+ * "EchoChamber" is the name of the package
+ * and "echo" is the address to access this class from the server
+ */
+@ServerEndpoint("/echo") 
+public class Server {
+    /**
+     * @OnOpen allows us to intercept the creation of a new session.
+     * The session class allows us to send data to the user.
+     * In the method onOpen, we'll let the user know that the handshake was 
+     * successful.
+     */
+    @OnOpen
+    public void onOpen(Session session){
+        System.out.println(session.getId() + " has opened a connection"); 
+        try {
+            session.getBasicRemote().sendText("Connection Established");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+ 
+    /**
+     * When a user sends a message to the server, this method will intercept the message
+     * and allow us to react to it. For now the message is read as a String.
+     */
+    @OnMessage
+    public void onMessage(String message, Session session){
+        System.out.println("Message from " + session.getId() + ": " + message);
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+ 
+    @OnClose
+    public void onClose(Session session){
+        System.out.println("Session " +session.getId()+" has ended");
+    }
+}
